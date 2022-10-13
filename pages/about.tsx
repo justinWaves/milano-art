6e119b39767a7ee4b3b98f5c779cf46deec561ab
justinWaves <juhstinn@gmail.com>
2022-10-13 14:55:44 -0700

    fixed contact menu bug in about section

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useState } from "react";
+
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import MeetMatt from "../components/MeetMatt";
+import ContactScreen from "../components/ContactScreen"
 
 function about() {
+  const [isContactScreenOpen, setIsContactScreenOpen] = useState(false);
+
+  const toggleContactMenu = () => {
+    setIsContactScreenOpen(!isContactScreenOpen);
+  };
+
   return (
     <div>
-      <Header />
+            {isContactScreenOpen ? (
+        <ContactScreen handleClose={toggleContactMenu} />
+      ) : (
+        <></>
+      )}
+        <Header openContact={toggleContactMenu} />
       <div className="h-screen">
         <MeetMatt />
       </div>
